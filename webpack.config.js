6e78f4acb1f06e72a6021f4650bfdbac009e0cf8
Syed Javeed <Syedjaveed.ix@gmail.com>
2024-01-16 02:16:17 +0530

    updated webpack config to take .env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+require('dotenv').config();
 
 module.exports = {
   mode: 'production',
@@ -9,4 +13,8 @@ module.exports = {
     filename: 'final.js',
   },
   target: 'node',
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.EnvironmentPlugin(['PORT', 'MONGO_URL', 'JWT_SECRET_KEY', 'JWT_EXPIRES_IN', 'USER_USERNAME', 'USER_PASSWORD']),
+  ],
 };
